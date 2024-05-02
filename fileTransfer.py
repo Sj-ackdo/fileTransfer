a12993055519dcac96d56b8c6912bbdff2fb985c
sj-ackdo <sjackdo@outlook.com>
2024-05-02 21:13:10 +0200

    test

---
+++ fileTransfer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+
+def start():
+    usbPy = os.popen("ls /dev/disk/by-id | grep '^usb.*part1$'")
+    usbPy = usbPy.read().strip("\n")
+    amountUsb = len(usbPy.split())
+    
+    usb_dict = {}
+    for i, x in enumerate(usbPy.split()):
+        usb_dict[i] = x
+    
+    if amountUsb > 1:
+        print( "**", amountUsb, "Storage USB('s) found**")
+        print("-----------------------------------------------------")
+        for b in range(amountUsb):
+            print(str(b + 1) + ".", usb_dict.get(b))
+        print("-----------------------------------------------------")    
+    elif amountUsb == 0:
+        print("*!* No USB's found! *!*")
+        exit()
+
+    usbChoice = input("Enter the number of the USB you want to use: ")
+    usbChoice = int(usbChoice) - 1
+    usbChoice = usb_dict.get(usbChoice)
+    if not os.path.exists("/media/usb"):
+        os.mkdir("/media/usb")
+    os.system("mount /dev/disk/by-id/" + usbChoice + " /media/usb")
+    
+    print("Which directory would you like to transfer to the USB?\n")
+    inputDir = input("Enter the directory: ").rstrip()
+    if os.path.exists(inputDir):
+        os.system("cp -rv " + inputDir+ " /media/usb")
+        print("Transfer completed...")
+
+
+
+start()
